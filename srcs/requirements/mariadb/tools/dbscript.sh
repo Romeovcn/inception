a4a7a44af741e2f29cb24dd0eb0d96abc6b2b329
romeovcn <vincent-romeo@hotmail.fr>
2024-02-14 19:12:35 +0100

    small fixes

--- srcs/requirements/mariadb/tools/dbscript.sh
+++ srcs/requirements/mariadb/tools/dbscript.sh
@@ -4,13 +4,14 @@ service mariadb start;
 
 sleep 5
 
-mariadb -u root -p${MARIADB_ROOT_PASSWORD} -e "CREATE DATABASE IF NOT EXISTS \`${MARIADB_DATABASE}\`;"
-mariadb -u root -p${MARIADB_ROOT_PASSWORD} -e "CREATE USER IF NOT EXISTS \`${MARIADB_USER}\`@'localhost' IDENTIFIED BY '${MARIADB_PASSWORD}';"
-mariadb -u root -p${MARIADB_ROOT_PASSWORD} -e "GRANT ALL PRIVILEGES ON \`${MARIADB_DATABASE}\`.* TO \`${MARIADB_USER}\`@'%' IDENTIFIED BY '${MARIADB_PASSWORD}';"
+mariadb -e "CREATE DATABASE IF NOT EXISTS \`${MARIADB_DATABASE}\`;"
+mariadb -e "CREATE USER IF NOT EXISTS \`${MARIADB_USER}\`@'localhost' IDENTIFIED BY '${MARIADB_PASSWORD}';"
+mariadb -e "GRANT ALL PRIVILEGES ON \`${MARIADB_DATABASE}\`.* TO \`${MARIADB_USER}\`@'%' IDENTIFIED BY '${MARIADB_PASSWORD}';"
+mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MARIADB_ROOT_PASSWORD}';"
 mariadb -u root -p${MARIADB_ROOT_PASSWORD} -e "FLUSH PRIVILEGES;"
 
 mysqladmin -u root -p${MARIADB_ROOT_PASSWORD} shutdown
 
-exec mysqld_safe
+echo "Database successfully created !"
 
-echo "MariaDB database and user were created successfully! "
+exec mysqld_safe
