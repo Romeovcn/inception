acb46f4edb7e6aa725fb342f28dbc45c522ebd53
romeovcn <vincent-romeo@hotmail.fr>
2024-02-17 13:32:00 +0100

    rebase

--- srcs/requirements/wordpress/tools/wpscript.sh
+++ srcs/requirements/wordpress/tools/wpscript.sh
@@ -3,25 +3,19 @@
 cd /var/www/html/wordpress
 
 if ! wp core is-installed --allow-root; then
+
 sleep 10
 
-wp config create --allow-root --dbname=${MARIADB_DATABASE} \
-			--dbuser=${MARIADB_USER} \
-			--dbpass=${MARIADB_PASSWORD} \
-			--dbhost=${MARIADB_HOST} \
+wp config create --allow-root --dbname=${MARIADB_DATABASE} --dbuser=${MARIADB_USER} \
+			--dbpass=${MARIADB_PASSWORD} --dbhost=${MARIADB_HOST} \
 			--url=https://${DOMAIN_NAME};
 
-wp core install --allow-root \
-			--url=https://${DOMAIN_NAME} \
-			--title=${SITE_TITLE} \
-			--admin_user=${ADMIN_USER} \
-			--admin_password=${ADMIN_PASSWORD} \
-			--admin_email=${ADMIN_EMAIL};
+wp core install --allow-root --url=https://${DOMAIN_NAME} \
+			--title=${SITE_TITLE} --admin_user=${ADMIN_USER} \
+			--admin_password=${ADMIN_PASSWORD} --admin_email=${ADMIN_EMAIL};
 
-wp user create --allow-root \
-			${USER1_LOGIN} ${USER1_MAIL} \
-			--role=author \
-			--user_pass=${USER1_PASS} ;
+wp user create --allow-root ${USER1_LOGIN} ${USER1_MAIL} \
+			--role=author --user_pass=${USER1_PASS} ;
 
 wp cache flush --allow-root
 
